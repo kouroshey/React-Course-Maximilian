ab420e8a55b531267fb2deb6f33e8d1df5d21147
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-08-03 10:16:26 +0330

    change path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import AddUser from './components/UI/AddUser'
+import AddUser from './components/Users/AddUser'
 const App = () => {
   return (
     <>
